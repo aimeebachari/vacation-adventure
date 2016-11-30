2e45ad5e24d6ff9f5aad1d0351803e194af5b3a2
Aimee Bachari <aimeebachari@gmail.com>
2016-11-30 08:58:39 -0500

    vacation-adventure

---
+++ vacation_adventure.rb
@@ -0,0 +1,9 @@
+number = rand(3) + 1
+
+if number == 1
+  puts "We're going sailing today!"
+elsif number == 2
+  puts "We're going to the animal reservation!"
+elsif number == 3
+  puts "We're going jet skiing!"
+end
